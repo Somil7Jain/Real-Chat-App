1d6f3c7b07ce4d815920a3f17596500854f44189
Somil Jain <jain.somil.jbp@gmail.com>
2021-07-08 22:09:58 +0530

    chat-app

---
+++ src/components/LoadingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./LoadingPage.css";
+
+export const LoadingPage = () => {
+  return (
+    <div className="bodyy">
+      <div className="loading_container">
+        <div className="loading"></div>
+        <h3>Loading...</h3>
+      </div>
+    </div>
+  );
+};
